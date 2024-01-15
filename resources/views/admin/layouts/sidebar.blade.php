87b65d71a1a0df4db6f165f019166440cb227132
farihanurhasin20 <fariha.hasin4424@gmail.com>
2024-01-15 14:33:41 +0600

    dekh

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
     <!-- Brand Logo -->
     <a href="{{ route('admin.dashboard') }}" class="brand-link">
         <img src="{{asset('admin-assets/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Opus E-commerce</span>
+        <span class="brand-text font-weight-light">PHQMM</span>
     </a>
     <!-- Sidebar -->
     <div class="sidebar">
