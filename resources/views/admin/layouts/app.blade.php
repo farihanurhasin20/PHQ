c42f2427c0e970f97fe7d69df081f7ac67410f93
farihanurhasin20 <fariha.hasin4424@gmail.com>
2024-01-15 13:54:31 +0600

    PHQ admin panel

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -56,10 +56,6 @@
 								<i class="fas fa-user-cog mr-2"></i> Settings
 							</a>
 							<div class="dropdown-divider"></div>
-							<a href="{{route('admin.showChangePassword')}}" class="dropdown-item">
-								<i class="fas fa-lock mr-2"></i> Change Password
-							</a>
-							<div class="dropdown-divider"></div>
 							<a href="{{route('admin.logout')}}" class="dropdown-item text-danger">
 								<i class="fas fa-sign-out-alt mr-2"></i> Logout
 							</a>
