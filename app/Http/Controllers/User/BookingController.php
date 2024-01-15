4fd69b22691882dc5ea8c1c81fcff9fe18f6076d
tasnin_siam <tasnin.siam@gmail.com>
2024-01-15 17:46:16 +0600

    booking order

--- app/Http/Controllers/User/BookingController.php
+++ app/Http/Controllers/User/BookingController.php
@@ -7,15 +7,19 @@ use App\Models\Booking;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
 // use BaconQrCode\Encoder\QrCode;
 
 class BookingController extends Controller
 {
     public function index()
     {
-        $user = Auth::user();
+
         
-        $bookings = Booking::where('user_id', $user->id)->get();
+        $today = Carbon::today();
+    
+        $bookings = Booking::whereDate('created_at', $today)
+        ->get();
         
         return response()->json(['data' => $bookings], 200);
     }
@@ -35,6 +39,17 @@ class BookingController extends Controller
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 200);
         }
+        $user = Auth::user();
+        $today = Carbon::today();
+        $bookings = Booking::where('user_id', $user->id)
+        ->whereDate('created_at', $today)
+        ->get()->first();
+
+        
+        if ($bookings != null) {
+            return response()->json(['message' => 'Already Booked'], 200);
+        }
+       
     
         $bookingData = $request->all();
     
