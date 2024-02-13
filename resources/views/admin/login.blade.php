0c5ab95655b890e8c2bb6ba10e972261df4cb1da
farihanurhasin20 <fariha.hasin4424@gmail.com>
2024-02-13 13:50:38 +0600

    all code

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,83 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>PHQMM :: Administrative Panel</title>
-		<!-- Google Font: Source Sans Pro -->
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-		<!-- Font Awesome -->
-		<link rel="stylesheet" href="{{asset('admin-assets/plugins/fontawesome-free/css/all.min.css')}}">
-		<!-- Theme style -->
-		<link rel="stylesheet" href="{{asset('admin-assets/css/adminlte.min.css')}}">
-		<link rel="stylesheet" href="{{asset('admin-assets/css/custom.css')}}">
-	</head>
-	<body class="hold-transition login-page">
-		<div class="login-box">
-			@include('admin.message')
-			<!-- /.login-logo -->
-			<div class="card card-outline card-primary">
-			  	<div class="card-header text-center">
-					<a href="#" class="h3">Administrative Panel</a>
-			  	</div>
-			  	<div class="card-body">
-					<p class="login-box-msg">Sign in to start your session</p>
-					<form action="{{route('admin.authenticate')}}" method="post">
-						@csrf
-				  		<div class="input-group mb-3">
-							<input type="number" name="mobile" id="mobile" class="form-control" @error('mobile') is-invalid @enderror placeholder="Mobile Number" >
-							<div class="input-group-append">
-					  			<div class="input-group-text">
-									<span class="fas fa-envelope"></span>
-					  			</div>
-							</div>
-				  		</div>
-						@error('email')
-							<p class="invalid-feedback">{{$message}}</p>
-						@enderror
-				  		<div class="input-group mb-3">
-							<input type="password" name="password" id="passoword" class="form-control" @error('password') is-invalid @enderror placeholder="Password" >
-							<div class="input-group-append">
-					  			<div class="input-group-text">
-									<span class="fas fa-lock"></span>
-					  			</div>
-							</div>
-				  		</div>
-						  @error('password')
-						  <p class="invalid-feedback">{{$message}}</p>
-					  @enderror
-				  		<div class="row">
-							<!-- <div class="col-8">
-					  			<div class="icheck-primary">
-									<input type="checkbox" id="remember">
-									<label for="remember">
-						  				Remember Me
-									</label>
-					  			</div>
-							</div> -->
-							<!-- /.col -->
-							<div class="col-4">
-					  			<button type="submit" class="btn btn-primary btn-block">Login</button>
-							</div>
-							<!-- /.col -->
-				  		</div>
-					</form>
-		  			<p class="mb-1 mt-3">
-				  		<a href="forgot-password.html">I forgot my password</a>
-					</p>					
-			  	</div>
-			  	<!-- /.card-body -->
-			</div>
-			<!-- /.card -->
-		</div>
-		<!-- ./wrapper -->
-		<!-- jQuery -->
-		<script src="{{asset('admin-assets/plugins/jquery/jquery.min.js')}}"></script>
-		<!-- Bootstrap 4 -->
-		<script src="{{asset('admin-assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-		<!-- AdminLTE App -->
-		<script src="{{asset('admin-assets/js/adminlte.min.js')}}"></script>
-		<!-- AdminLTE for demo purposes -->
-		{{-- <script src="{{asset('admin-assets/js/demo.js')}}"></script> --}}
-	</body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHQMM :: Administrative Panel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .login-container {
+            max-width: 400px;
+            border-radius: 10px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 40px;
+        }
+        .login-container h2 {
+            font-family: math ;
+            margin-bottom: 30px;
+            text-align: center;
+        }
+        .form-control {
+            border-radius: 20px;
+        }
+        .login-btn {
+            border-radius: 20px;
+            font-weight: 600;
+            background-color: #0056b3;
+            border-color: #0056b3;
+        }
+        .login-btn:hover {
+            background-color: #004080;
+            border-color: #004080;
+        }
+        .logo-container {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="login-container">
+        <div class="logo-container">
+            <img src="{{asset('admin-assets/img/login.png')}}" alt="Logo" class="logo-img" style="max-width: 60px;">
+        </div>
+        <h2>Administrative Panel</h2>
+        <form action="{{route('admin.authenticate')}}" method="post">
+            @csrf
+            <div class="form-group">
+                <input type="number" name="mobile" id="mobile" class="form-control" placeholder="Mobile Number">
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" id="password" class="form-control" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary btn-block login-btn">Login</button>
+            <br>
+        </form>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
